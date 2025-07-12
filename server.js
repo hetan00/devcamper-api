51a84ff0028e34631f1a92b22914d5097d7554d3
HetanKamalkumar <hetan_28@hotmail.com>
2025-07-12 01:00:27 +0100

    Creating Routes

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 //Load env vars //load
-dotenv.config({path: '/config/config.env'})
+dotenv.config({path: './config/config.env'})
 
 const app = express()
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'})
+} )
+
+app.get('/api/v1/bootcamps:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Show single bootcamp ${req.params.id}`})
+} )
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Create new bootcamp'})
+} )
+
+app.put('/api/v1/bootcamps:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
+} )
+
+app.delete('/api/v1/bootcamps', (req, res) => {
+res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`})
+})
+
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
