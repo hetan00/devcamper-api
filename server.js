137d979c147f12127abe1140c3e307326563b609
HetanKamalkumar <hetan_28@hotmail.com>
2025-07-28 23:35:06 +0100

    started using mongodb. Will implement it soon into app

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
 
 //Load env vars //load
 dotenv.config({path: './config/config.env'})
 
+//Connect to database
+connectDB()
+
+// Route files
+import bootcamps from './routes/bootcamps.js'
+
 const app = express()
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
-} )
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
-app.get('/api/v1/bootcamps:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Show single bootcamp ${req.params.id}`})
-} )
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: 'Create new bootcamp'})
-} )
-
-app.put('/api/v1/bootcamps:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
-} )
-
-app.delete('/api/v1/bootcamps', (req, res) => {
-res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`})
-})
-
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    //Close server & exit process
+    server.close(() => process.exit(1))
+})
