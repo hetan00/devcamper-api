3294f089936c1aa078ab653550b9337505b8354f
HetanKamalkumar <hetan_28@hotmail.com>
2025-08-03 21:42:23 +0100

    create a bootcamp model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import connectDB from './config/db.js'
+import colors from 'colors'
 
 //Load env vars //load
 dotenv.config({path: './config/config.env'})
@@ -24,12 +25,12 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 // Handle unhandled promise rejections
 
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     //Close server & exit process
     server.close(() => process.exit(1))
 })
