38e7035bf59c514557c5a9e9579e507291de6061
HetanKamalkumar <hetan_28@hotmail.com>
2025-08-19 01:04:06 +0100

    custom errorResponse class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 //import Bootcamp from '../models/Bootcamp.js'
 const Bootcamp = require('../models/Bootcamp.js')
+const ErrorResponse = require('../utils/errorResponse.js')
 
 // @desc Get all bootcamps
 //  @route GET /api/v1/bootcamps
@@ -23,12 +24,15 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id)
 
     if(!bootcamp) {
-       return res.status(400).json({ success: false})
+       return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404))
+
     }
 
      res.status(200).json({success: true, data: bootcamp})
-   } catch (error) {
-     res.status(400).json({ success: false})
+   } catch (err) {
+     //res.status(400).json({ success: false})
+     next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404))
+
    }
 }
 
@@ -44,7 +48,7 @@ exports.createBootcamp = async(req, res, next) => {
         data: bootcamp
     })   
     } catch (err) {
-        res.status(400).json({success: false})
+      res.status(400).json({success: false})
     }
     
 }
--- server.js
+++ server.js
@@ -1,39 +1,44 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import connectDB from './config/db.js'
-import colors from 'colors'
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
 
-//Load env vars //load
-dotenv.config({path: './config/config.env'})
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
-//Connect to database
-connectDB()
+// Connect to database
+connectDB();
 
 // Route files
-import bootcamps from './routes/bootcamps.js'
+const bootcamps = require('./routes/bootcamps');
 
-const app = express()
+const app = express();
 
-//Body parser 
-app.use(express.json())
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 5000
+app.use(errorHandler);
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
 
 // Handle unhandled promise rejections
-
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`.red)
-    //Close server & exit process
-    server.close(() => process.exit(1))
-})
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
