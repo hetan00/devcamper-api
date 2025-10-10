b7ca524f3afb29404331a70db65544eca1ebcddc
HetanKamalkumar <hetan_28@hotmail.com>
2025-10-10 22:46:49 +0100

    middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,8 +6,12 @@ const {
     createBootcamp, 
     updateBootcamp, 
     deleteBootcamp, 
-    getBootcampsInRadius
+    getBootcampsInRadius,
+    bootcampPhotoUpload
 } = require('../controllers/bootcamps')
+
+const Bootcamp = require('../models/Bootcamp')
+const advancedResults = require('../middleware/advancedResults')
 
 // Include other resource routers
 
@@ -20,9 +24,12 @@ router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
-router.route('/').get(getBootcamps).post(createBootcamp)
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp)
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+
+router.route('/:id/photo').put(bootcampPhotoUpload)
+
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const fileupload = require('express-fileupload')
 const colors = require('colors');
 const errorHandler = require('./middleware/error');
 
@@ -24,6 +26,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+
+
+// File uploading
+app.use(fileupload())
+
+// Set static folder 
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
