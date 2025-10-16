4f30626220906dfc99e7bb01350040c4137477fa
HetanKamalkumar <hetan_28@hotmail.com>
2025-10-16 01:47:24 +0100

    token

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,6 +17,9 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
+    // Create token 
+    const token = user.getSignedJwtToken() 
 
-    res.status(200).json({success: true})
+
+    res.status(200).json({success: true, token})
 })
