2cf8a529528eafdf54023c15f8cc4d3dbd5aaf93
HetanKamalkumar <hetan_28@hotmail.com>
2025-10-17 01:06:43 +0100

    sending jwt in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse.js')
 const User = require('../models/User.js')
 
 // @desc Register user
-// @route GET /api/v1/auth/register
+// @route POST /api/v1/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,9 +17,57 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
-    // Create token 
-    const token = user.getSignedJwtToken() 
-
-
-    res.status(200).json({success: true, token})
+    sendTokenResponse(user, 200, res)
 })
+
+// @desc Login User
+// @route POST /api/v1/auth/login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // Validate email and password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials', 401))
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if(!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401))
+    }
+
+    sendTokenResponse(user, 200, res)
+})
+
+// Get token from model, create cookie and send response 
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken()
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV === 'production') {
+        options.secure = true
+    }
+    
+    res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+        success: true,
+        token
+    })
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
 const colors = require('colors');
 const errorHandler = require('./middleware/error');
 
@@ -22,6 +23,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser())
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
